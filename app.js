3ed9253e6048840d5436a6ef82b481592f94270a
omarggiorgini <omar.garcia.giorgini@gmail.com>
2023-11-17 10:28:08 -0300

    add cors middleware

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mailRoutes = require('./routes/index');
+const cors = require('cors');
 
 const app = express();
+
+// Use CORS middleware
+app.use(cors());
 
 app.use(express.json());
 app.use('/', mailRoutes);
